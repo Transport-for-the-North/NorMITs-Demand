ac947c9544ede2c666a9d0a80e6618fc2f34a447
Humza-TFN <humza.ali@transportforthenorth.com>
2021-03-08 17:00:35 +0000

    Final additions to segmentator privacy masker
    - Changed folder to /normits_demand/...
    - Added descriptions to all functions
    - Finished segmentator_privacy_masker function

--- normits_demand/mnd_processing/run_mdd_functions.R
+++ normits_demand/mnd_processing/run_mdd_functions.R
@@ -9,17 +9,14 @@ preprocess_mnd(mdd_path = "Y:/Mobile Data/LOT 2/Data/MPD Mode-Purpose-TypeOfDay-
                output_path = "Y:/Mobile Data/LOT 2/Data/MPD Mode-Purpose-TypeOfDay-TimeOfDay/")
 
 
+# 2) privacy masking segmenting tool --------------------------------------
 
 segmentator_privacy_masker(mdd_path = "C:/Users/Pluto/Documents/MPD/lot2_long_od.csv",
                            output_path = "C:/Users/Pluto/Documents/MPD/",
-                           add_privacy = TRUE, # TRUE if privacy infill
-                           flatten_wide = TRUE) # TRUE if flatten to wide
-
-segmentator_privacy_masker(mdd_path = "Y:/Mobile Data/LOT 2/Data/MPD Mode-Purpose-TypeOfDay-TimeOfDay/lot2_long_od.csv",
-                           output_path = "C:/Users/HumzaAli/Documents/MPD/",
-                           add_privacy = TRUE,
-                           flatten_wide = TRUE)
-
+                           segments = c("m", "tpp", "p", "d"),
+                           add_privacy = FALSE,
+                           flatten_wide = TRUE,
+                           long_segments = TRUE,
+                           long_unsegment = FALSE)
 
 # TODO: Add functionality to bind rows and save privacy masked big long format so not in segments!
-
