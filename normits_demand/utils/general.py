25d3dbf3f610633947384e38cd308fd5300b4326
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2021-04-06 12:54:58 +0100

    Update general.py
    Added sum_df_dict()

--- normits_demand/utils/general.py
+++ normits_demand/utils/general.py
@@ -18,6 +18,7 @@ import re
 import shutil
 import random
 import inspect
+import operator
 
 
 import pandas as pd
@@ -3213,3 +3214,50 @@ def split_hb_nhb_purposes(purposes: List[int]) -> Tuple[List[int], List[int]]:
                 "%s is not a valid HB or NHB purpose" % str(p)
             )
     return hb_p, nhb_p
+
+
+def sum_df_dict(dict_list: List[Dict[Any, pd.DataFrame]],
+                non_sum_cols: List[str],
+                sum_keys: List[Any] = None,
+                ) -> Dict[Any, pd.DataFrame]:
+    """
+    Sums the dataframes in dict_list on matching keys
+
+    Parameters
+    ----------
+    dict_list:
+        A list of the dictionaries to sum
+
+    non_sum_cols:
+        A list of the column names that should not be summed
+
+    sum_keys:
+        A list of the keys to sum on. If left as None, will infer the keys
+        to use based on those existing in dict_list[0].
+
+    Returns
+    -------
+    summed_df_dict:
+        A dictionary with sum_keys, containing the sum all vales at
+        that key in dict_list.
+    """
+    # Init
+    sum_keys = list(dict_list[0].keys()) if sum_keys is None else sum_keys
+
+    # CHeck all keys exist
+    for sub_dict in dict_list:
+        if not all([k in sub_dict for k in sum_keys]):
+            raise KeyError(
+                "Cannot find all the sum_keys in all the dictionaries."
+            )
+
+    # Sub across keys
+    ret_dict = dict()
+    for k in sum_keys:
+        dfs = [d[k] for d in dict_list]
+        dfs = [df.set_index(non_sum_cols) for df in dfs]
+
+        sum_df = functools.reduce(operator.add, dfs)
+        ret_dict[k] = sum_df.reset_index()
+
+    return ret_dict
