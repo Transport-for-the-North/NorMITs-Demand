168c07da8d2bccc1c699f80f1f0e753d0cb901bf
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2021-05-10 11:11:13 +0100

    Update run_segment_disaggregator.py
    Rename the TST outputs for EFS

--- run_segment_disaggregator.py
+++ run_segment_disaggregator.py
@@ -1,6 +1,8 @@
 
 import os
+import shutil
 
+from normits_demand.utils import file_ops
 from normits_demand.distribution import segment_disaggregator as sd
 
 IMPORT_DRIVE = "I:/"
@@ -12,6 +14,7 @@ ITER_NAME = 'iter4'
 
 # BACKLOG: Adjust output of TST so EFS can take in as is
 #  labels: EFS, QoL Updates
+
 
 def main():
     global MODEL_NAME
@@ -93,8 +96,66 @@ def main():
         max_bs_loops=200,
         mp_threads=-1,
         export_original=True,
-        export_furness=False)
+        export_furness=False
+    )
+
+
+def rename_tst_outputs():
+    mat_dir = r'I:\NorMITs Synthesiser\Norms_2015\iter4\Norms 15 Post ME\tfn_seg_pa'
+    ca_vals = [1, 2]
+    soc_vals = [0, 1, 2, 3]
+    ns_vals = [1, 2, 3, 4, 5]
+
+    # ## MOVE REPORTS INTO SUB FOLDER ## #
+    report_dir = os.path.join(mat_dir, 'reports')
+    file_ops.create_folder(report_dir, verbose=False)
+
+    # Find all the reports and move
+    csvs = [x for x in os.listdir(mat_dir) if '.csv' in x]
+    reports = [x for x in csvs if 'report' in x]
+
+    for report in reports:
+        src = os.path.join(mat_dir, report)
+        dst = os.path.join(report_dir, report)
+        shutil.move(src, dst)
+
+    # ## RENAME THE MATS ## #
+    mats = [x for x in os.listdir(mat_dir) if '.csv' in x]
+
+    for old_name in mats:
+        new_name = None
+
+        # Add in the year
+        mid_name = old_name.replace('_enhpa_', '_pa_yr2018_')
+
+        # Swap soc/ns and ca
+        for ca in ca_vals:
+            ca_str = 'ca%s' % ca
+            if ca_str not in mid_name:
+                continue
+
+            for s in soc_vals:
+                soc_str = 'soc%s' % s
+                if soc_str in mid_name:
+                    old = '_%s_%s' % (ca_str, soc_str)
+                    new = '_%s_%s' % (soc_str, ca_str)
+                    new_name = mid_name.replace(old, new)
+
+            for n in ns_vals:
+                ns_str = 'ns%s' % n
+                if ns_str in mid_name:
+                    old = '_%s_%s' % (ca_str, ns_str)
+                    new = '_%s_%s' % (ns_str, ca_str)
+                    new_name = mid_name.replace(old, new)
+
+        if new_name is None:
+            raise ValueError("Couldn't rename %s" % old_name)
+
+        src = os.path.join(mat_dir, old_name)
+        dst = os.path.join(mat_dir, new_name)
+        shutil.move(src, dst)
 
 
 if __name__ == '__main__':
    main()
+   # rename_tst_outputs()
