4dd62ec20a34116a9f1761f37f56e2c0efa2b8a1
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2021-07-02 14:00:48 +0100

    Formatting updates

--- test_notem_prod.py
+++ test_notem_prod.py
@@ -17,12 +17,17 @@ from normits_demand.models import notem_production_model as notem
 
 # GLOBAL VARIABLES
 # I Drive Path locations
-POPULATION_PATH = {2018: r"I:\NorMITs Land Use\base_land_use\iter3b\outputs\land_use_output_tfn_msoa1.csv"}
+POPULATION_PATH = {
+    2018: r"I:\NorMITs Land Use\base_land_use\iter3b\outputs\land_use_output_tfn_msoa1.csv",
+    # 2033: r"I:\NorMITs Land Use\future_land_use\iter3b\scenarios\NTEM\land_use_2033_pop.csv",
+    # 2040: r"I:\NorMITs Land Use\future_land_use\iter3b\scenarios\NTEM\land_use_2040_pop.csv",
+    # 2050: r"I:\NorMITs Land Use\future_land_use\iter3b\scenarios\NTEM\land_use_2050_pop.csv",
+}
 TRIP_RATES_PATH = r"I:\NorMITs Demand\import\NoTEM\HB_Productions\hb_trip_rates_v1.9.csv"
 MODE_TIME_SPLITS_PATH = r"I:\NorMITs Demand\import\NoTEM\HB_Productions\hb_mode_time_split_v1.9.csv"
-constraint_PATH = {2018: r"I:\NorMITs Land Use\base_land_use\iter3b\outputs\land_use_output_tfn_msoa1.csv"}
-export_path = r"C:\Data\Nirmal_Atkins"
-# export_path = "E:/"
+constraint_PATH = POPULATION_PATH.copy()
+# export_path = r"C:\Data\Nirmal_Atkins"
+export_path = "E:/Productions"
 
 
 def main():
@@ -33,7 +38,14 @@ def main():
         constraint_PATH,
         export_path
     )
-    hb = hb_prod.run()
+
+    hb_prod.run(
+        export_pure_demand=True,
+        export_fully_segmented=False,
+        export_notem_segmentation=True,
+        export_reports=True,
+        verbose=True,
+    )
 
 
 if __name__ == '__main__':
