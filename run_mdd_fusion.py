dc216c0dacc36c05563192531af6ae583004d5e8
PeterSMorris <Peter.S.Morris@Jacobs.com>
2021-09-13 17:31:40 +0100

    Scoping Update

---
+++ run_mdd_fusion.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: Mon September 13 2021
+Updated on:
+
+Original author: Peter Morris
+Last update made by:
+Other updates made by:
+
+File purpose:
+Scoping test runs of MDD fusion
+"""
+
+import normits_demand as nd
+from normits_demand import fusion_constants as consts
+
+# TODO: scope mdd_pre_processing structure
+
+def main():
+    # TODO: scope out main function
+
+
+
+
+
+# TODO: locate un-expanded MDD demand
+
+
+if __name__ == '__main__':
+    main()
