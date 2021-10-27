559f5e41bb2b9b72a002cb69430aff01fc01f500
PeterSMorris <Peter.S.Morris@Jacobs.com>
2021-10-27 15:34:02 +0100

    Update mdd_pre_process.py

--- normits_demand/mdd_fusion/mdd_pre_process.py
+++ normits_demand/mdd_fusion/mdd_pre_process.py
@@ -27,6 +27,7 @@ GV_Demand_loc = 'Y:/NoHAM/7.Network_Builder/4.NoHAM_IPBA-June21/Demand'
 BusCar_Demand_loc = 'I:/Transfer/External Model OD'
 LGV_Dist_loc = 'Y:/NoHAM/17.TUBA_Runs/-TPT/Skims/RefDist_Skims'
 gv_nts_loc = 'Y:/NTS/outputs'
+MPD_Processing = 'Y:/Mobile Data/Processing'
 
 class MDDPreProcess:
     # TODO: start then finish this
@@ -127,12 +128,13 @@ class MDDPreProcess:
             Calculate mode proportions by purpose for car, bus, LGV, HGV
             
     """
+
     #Import LGV distance skim - convert to numpy array
     lgvDist = pd.read_csv(LGV_Dist_loc + '/NoHAM_Base_2018_TS2_v106_Dist_LGV.csv', header=None, index_col=None, low_memory=False)
-    
     lgvDist2 = lgvDist.pivot(index=0, columns=1, values=2)
     lgvDist2 = lgvDist2.to_numpy()
-    
+    lgvDist3 = np.nan_to_num(lgvDist2)
+
     #Import LGV tour proportions
     lgv_tour = pd.read_excel(gv_nts_loc + '/van_tour_props_check.xlsx', sheet_name='van_tour_props', header=0, index_col=None, usecols=list(range(0,8)))
     
@@ -140,9 +142,80 @@ class MDDPreProcess:
     lgv_oc = pd.read_excel(gv_nts_loc + '/van_co_check.xlsx', sheet_name='van_co2', header=0, index_col=None)
     
     #Import zone-GOR lookup
+    znList = pd.read_csv(MPD_Processing +
+                         '/NoHAM_zone-GOR.csv',
+                         header=0)
     
-    
-    #Assign TLD band to dist skim
-    
-    
+    # Join tour props to zone list
+    znList_tour = pd.merge(znList, lgv_tour, how='right', left_on=['TFN_GOR2'], right_on=['agg_gor_from'])
+    # Remove extra data columns
+    znList_tourtidy = znList_tour[['Zone', 'p', 'trip_direction', 'start_time', 'prop']]
+    # Import purp list
+    toMndPurpList = pd.read_csv(MPD_Processing +
+                         '/toMndPurp.csv',
+                         header=0)
+    # add mnd purpose to tour props
+    znList_tourTidyPurp = pd.merge(znList_tourtidy, toMndPurpList, how='left', on=['p', 'trip_direction'])
+    # Reset index to mnd purpose & start time
+    znList_tourTidyPurp = znList_tourTidyPurp.set_index(['start_time', 'MndPurp'])
+    # Print example zone list
+    # print(znList_tourTidyPurp.loc[([1], [3]), ['Zone', 'prop']].sort_values(by=['Zone']).to_numpy)
+    # znList_tourTidyPurp_check = znList_tourTidyPurp.loc[([1], [3]), ['Zone', 'prop']].sort_values(by=['Zone']).to_numpy
+    # znList_tourTidyPurp.loc[([1], [3]), ['Zone', 'prop']].to_csv(MPD_Processing + '/LGV_prop.csv', index=False, header=True)
+    """ create test array
+    temp_array = np.array([[1, 2, 3], [2, 4, 6], [3, 6, 9]])
+    temp_array2 = np.array([0, 1, 2])
+    temp_array3 = temp_array * temp_array2[:, np.newaxis]
+    print(temp_array3)
+    # create test nested dictionaries
+    dctgv_vehtp = {}
+    for md in dctmode:
+        dctgv_vehtp[md] = {}
+        for wd in dctday:
+            dctgv_vehtp[md][wd] = {}
+            for tp in dcttp:
+                dctgv_vehtp[md][wd][tp] = temp_array
+    """
+    # duplicate 24hr into tps
+    dctlgv_tours= {}
+    dctlgv_tours[4] = {}
+    for wd in dctday:
+        dctlgv_tours[4][wd] = {}
+        for tp in dcttp:
+            dctlgv_tours[4][wd][tp] = {}
+            for p in mndPurp:
+                dctlgv_tours[4][wd][tp][p] = {}
+                dctlgv_tours[4][wd][tp][p] = dctgv_vehtp[4][wd][5]
+
+    for wd in dctday:
+        for tp in dcttp:
+            for p in mndPurp:
+                tourProp1 = znList_tourTidyPurp.loc[([tp], [p]), ['Zone', 'prop']].sort_values(by=['Zone'])
+                tourProp2 = tourProp1.head(2770)
+                tourProp3 = tourProp2['prop'].to_numpy()
+                dctlgv_tours[md][wd][tp][p] = dctlgv_tours[md][wd][tp][p] * tourProp3[:, np.newaxis]
+
+    # Import dbands
+    dbandList = pd.read_csv(MPD_Processing +
+                         '/dbands.csv',
+                         header=0)
+    # Covert distance matrix to dband integers
+    # TODO: update dband list to correct bands
+    lgvDistBand = np.digitize(lgvDist3, dbandList['Min'])
+    # Loop indexed lgv oc list and convert dband integers with occupancy factor
+    # TODO: find code used to create & index lgv_oc
+    # TODO: duplicate dband integer matrices into nested purpose dict
+    # TODO: build loop to convert by purpose
+    for tp:
+        for band, purp, oc in lgv_oc[lgv_oc['purpose'] == 'HBW'].itertuples(index=False):
+            print(type(band), oc)
+            if purp in ['HBW'] p = 1 ifelse
+            lgvDistBand[tp][p] = np.where(lgvDistBand == band, oc, lgvDistBand)
+    # TODO: apply occupancy adjustment to TP LGVs
+
+    # TODO: get and format Bus data
+    # TODO: build all mode dictionary
+    # TODO: check tld by mode/purpose
+    # TODO: check mode share by origin/purpose
+
     
