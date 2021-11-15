75cfe08d8fdf5db0b0a671312d73dd0519876e4e
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2021-11-15 12:15:05 +0000

    WIP: Tour Props rewrite

--- run_helper_scripts/tms_tour_prop.py
+++ run_helper_scripts/tms_tour_prop.py
@@ -12,65 +12,99 @@ TMS tour proportions Generation
 
 # Built-Ins
 import os
+import sys
 
 # Third party imports
 import pandas as pd
 
 # Local Imports
+sys.path.append("..")
 import normits_demand as nd
 from normits_demand.utils import pandas_utils as pd_utils
 from normits_demand.utils import general as du
 from normits_demand.core.data_structures import DVector
 
-## GLOBALS ##
+# ## GLOBALS ## #
 modes = [3]
 years = [2018]
+zoning_system = "noham"
+
 phi_import_folder = r"I:\NorMITs Demand\import\phi_factors"
 notem_import_folder = r"I:\NorMITs Demand\NoTEM\iter4.2\SC01_JAM\hb_productions"
 phi_fname = "mode_%d_fhp_tp_pa_to_od.csv"
 prod_vec_fname = "hb_msoa_notem_segmented_%d_dvec.pkl"
-zoning_system = "noham"
 zone_translate_dir = r"I:\NorMITs Demand\import\zone_translation\one_to_one"
 
-
 out_fname = "hb_tour_proportions_yr%d_p%d_m%d.pkl"
-out_folder = r"I:\NorMITs Demand\import\noham\pre_me_tour_proportions\example_new"
+# out_folder = r"I:\NorMITs Demand\import\noham\pre_me_tour_proportions\example_new"
+out_folder = r'E:\test'
 
 
 def tms_tour_prop():
+
     for year in years:
         for mode in modes:
-            # Read phi factors for every mode
+
+            # ## GRAB PHI FACTORS BY MODE ## #
             phi_file = phi_fname % mode
             phi_df = pd.read_csv(os.path.join(phi_import_folder, phi_file))
 
-            purpose = phi_df['purpose_from_home'].drop_duplicates().reset_index(drop=True)
-            phi_dict = {}
-            # Convert the phi factors to the required format for each purpose
-            for p in purpose:
-                phi_sub_df = pd_utils.filter_df(phi_df, {'purpose_from_home': p})
-                phi_pivot = pd.pivot_table(phi_sub_df, values='direction_factor', index='time_from_home',
-                                           columns='time_to_home', aggfunc='sum')
-                phi_pivot = phi_pivot.head(-2)
-                phi_pivot = phi_pivot.drop(phi_pivot.columns[[4, 5]], axis=1)
-                phi_dict[p] = phi_pivot.to_numpy()
+            # Rename cols to match notem
+            rename = {
+                'purpose_from_home': 'p',
+                'time_from_home': 'tp',
+                'time_to_home': 'phi',
+            }
+            phi_df = phi_df.rename(columns=rename)
 
-            print(phi_dict)
-            ## TP Splits##
+            # Drop unneeded tps
+            mask = (
+                phi_df['tp'].isin([1, 2, 3, 4])
+                & phi_df['phi'].isin([1, 2, 3, 4])
+            )
+            phi_df = phi_df[mask].copy()
 
+            # Pivot
+            phi_df = phi_df.pivot(
+                index=['p', 'tp'],
+                columns=['phi'],
+            ).reset_index()
+
+            # ## GRAB TP SPLITS BY PURPOSE AND ZONE ## #
             notem_file = prod_vec_fname % year
-            # Convert it to the required segmentation
-            return_seg = nd.get_segmentation_level("hb_p_6tp")
-            # Read the production vector
-            notem_dvec = nd.from_pickle(os.path.join(notem_import_folder,notem_file))
-            # Aggregate to the required segmentation
-            notem_dvec = notem_dvec.aggregate(return_seg)
+            notem_dvec = nd.from_pickle(os.path.join(notem_import_folder, notem_file))
 
-            zon_sys = nd.get_zoning_system(zoning_system)
-            notem_df = notem_dvec.translate_zoning(new_zoning=zon_sys, weighting="population").to_df()
-            zon_col = "%s_zone_id" % zoning_system
-            notem_df['uniq_id'] = pd_utils.str_join_cols(notem_df, [zon_col, 'p'])
+            # Convert to needed segments and translate
+            week_seg = nd.get_segmentation_level("hb_p_tp_week")
+            wday_seg = nd.get_segmentation_level("hb_p_tp_wday")
+            zoning = nd.get_zoning_system(zoning_system)
+
+            notem_dvec = notem_dvec.aggregate(week_seg)
+            notem_dvec = notem_dvec.subset(wday_seg)
+            notem_df = notem_dvec.translate_zoning(zoning, weighting="population").to_df()
+
+            # Adjust factors back to 1
+            notem_df['sum'] = notem_df.groupby([zoning.col_name, 'p'])['val'].transform('sum')
+            notem_df['val'] /= notem_df['sum']
+            notem_df = notem_df.drop(columns='sum')
+
+            print(phi_df)
             print(notem_df)
+
+            full_df = pd.merge(
+                left=notem_df,
+                right=phi_df,
+                how='left',
+                on=['p', 'tp']
+            )
+
+            print(full_df)
+
+
+
+            exit()
+
+
             uniq = notem_df['uniq_id'].drop_duplicates().reset_index(drop=True)
             tp_split_dict = {}
             for q in uniq:
