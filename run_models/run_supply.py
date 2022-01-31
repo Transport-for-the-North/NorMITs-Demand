ff6b7e0471a3692445d2de00233574457e0f43ab
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2022-01-31 13:01:00 +0000

    Update supply to run from a config file

--- run_models/run_supply.py
+++ run_models/run_supply.py
@@ -10,15 +10,27 @@ Other updates made by:
 File purpose:
 Master run file to run NorMITs Supply
 """
+# Built-Ins
+import sys
 
+# Third Party
+
+# Local Imports
+sys.path.append("..")
 from normits_demand.models.supply import NorMITsSupply
 
-user = "prjt_normits_supply_ed@tfn-gis-server"
-password = "prjt_normits_supply_ed"
-host = "10.1.2.6"
-port = "5432"
-database = "gis_db"
-query_fname = "prjt_normits_supply.skim_master"
+# import the local config
+try:
+    import supply_config
+except ModuleNotFoundError as e:
+    msg = (
+        "Unable to import the config file. Has it been defined? "
+        "Expecting a file named 'supply_config.py' in the current directory. "
+        "If none exists, make a copy of 'supply_config.py-example', rename, "
+        "and define all arguments in there."
+    )
+    raise ModuleNotFoundError(msg) from e
+
 scenario_id = 2
 users_id = 1
 value_id = 2
@@ -28,13 +40,18 @@ export_home = r"I:\NorMITs Supply\Test"
 
 
 def main():
+    query_fname_parts = [
+        supply_config.SUPPLY_SCHEMA_NAME,
+        supply_config.SUPPLY_MASTER_TABLE,
+    ]
+
     p = NorMITsSupply(
-        user=user,
-        password=password,
-        server=host,
-        database=database,
-        port=port,
-        query_fname=query_fname,
+        user=supply_config.USERNAME,
+        password=supply_config.PASSWORD,
+        server=supply_config.DB_HOST_IP,
+        database=supply_config.DB_NAME,
+        port=supply_config.DB_HOST_PORT,
+        query_fname='.'.join(query_fname_parts),
         scenario_id=scenario_id,
         users_id=users_id,
         value_id=value_id,
@@ -42,6 +59,8 @@ def main():
         time_period=time_period,
         export_home=export_home,
     )
+    print("DONE")
+    exit()
     p.run(od_to_pa_conversion=False)
 
 
---
+++ run_models/supply_config.py-example.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: 31/01/2022
+Updated on:
+
+Original author: Ben Taylor
+Last update made by:
+Other updates made by:
+
+File purpose:
+Store the config for running normits_supply
+"""
+# Built-Ins
+
+# Third Party
+
+# Local Imports
+
+USERNAME = "username"
+PASSWORD = "password"
+DB_HOST_IP = "1.1.1.1"
+DB_HOST_PORT = "1111"
+DB_NAME = "db_name"
+SUPPLY_SCHEMA_NAME = "prjt_normits_supply"
+SUPPLY_MASTER_TABLE = "skim_master"
