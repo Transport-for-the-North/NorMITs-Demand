3bf93306f6db6f49877fe902973012acc2c3cacc
Matt <matthew.buckley@wsp.com>
2022-04-08 16:40:35 +0100

    AutoName enum class
    Added Enum sub-class which automatically sets the enum value to the same
    as the enum name when using `enum.auto`.

--- normits_demand/core/enumerations.py
+++ normits_demand/core/enumerations.py
@@ -27,6 +27,14 @@ from typing import List
 import normits_demand as nd
 
 
+class AutoName(enum.Enum):
+    """Enum class to automatically use the Enum name for it's value."""
+
+    @staticmethod
+    def _generate_next_value_(name, start, count, last_values):
+        del start, count, last_values  # Unused
+        return name
+
 @enum.unique
 class Mode(enum.Enum):
     WALK = 'walk'
