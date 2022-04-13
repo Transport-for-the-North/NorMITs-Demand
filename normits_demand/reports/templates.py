1fd0582663a9a393d89a01235a3b92a0b5828031
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2022-04-13 16:26:13 +0100

    Added class to complement report template

--- normits_demand/reports/templates.py
+++ normits_demand/reports/templates.py
@@ -16,11 +16,21 @@ import os
 import enum
 import pathlib
 import shutil
+import dataclasses
+
+from typing import Optional
+from dataclasses import InitVar
+
 
 # Third Party
+import pandas as pd
 
 # Local Imports
+# pylint: disable=import-error
+from normits_demand import core as nd_core
+from normits_demand import logging as nd_log
 from normits_demand.utils import general as du
+# pylint: enable=import-error
 
 
 @enum.unique
@@ -62,3 +72,78 @@ class ReportTemplates(enum.Enum):
 
         shutil.copy(src=src, dst=dst)
 
+
+@dataclasses.dataclass
+class DistributionModelMatrixReportSectorData:
+    """Helper class to store and manipulate sector output cols
+
+    To be used in conjunction with
+    ReportTemplates.DISTRIBUTION_MODEL_MATRIX
+
+    Attributes
+    ----------
+    sector_data:
+        A pandas DataFrame containing all the sector data.
+        Must contain the required columns, as defined in
+        `self.required_cols()`
+
+    from_zone_col:
+        The name of the column in `sector_data` which contains the from zone
+        name.
+
+    to_zone_col:
+        The name of the column in `sector_data` which contains the to zone
+        name.
+
+    val_col:
+        The name of the column in `sector_data` which contains the data values.
+
+    p_col:
+        The name of the column in `sector_data` which contains the purpose
+        segment value.
+
+    m_col:
+        The name of the column in `sector_data` which contains the purpose
+        segment value.
+
+    ca_col:
+        The name of the column in `sector_data` which contains the purpose
+        segment value.
+    """
+    sector_data: pd.DataFrame
+    from_zone_col: str
+    to_zone_col: str
+    val_col: str
+
+    p_col: Optional[str] = 'p'
+    m_col: Optional[str] = 'm'
+    ca_col: Optional[str] = 'ca'
+    segment_default_value: InitVar[int] = -1
+
+    def __post_init__(self, segment_default_value: int):
+        # Add in any columns that don't exist
+        for col_name in [self.p_col, self.m_col, self.ca_col]:
+            if col_name not in self.sector_data.columns:
+                self.sector_data[col_name] = segment_default_value
+
+        # Force sector data into the right order
+        self.sector_data = self.sector_data.reindex(columns=self.col_order())
+
+    def col_order(self):
+        """Get a list of the columns in their required output order"""
+        return [
+            self.from_zone_col,
+            self.to_zone_col,
+            self.p_col,
+            self.m_col,
+            self.ca_col,
+            self.val_col,
+        ]
+
+    def required_cols(self):
+        """Get a list of the required columns for the sector report"""
+        return [
+            self.from_zone_col,
+            self.to_zone_col,
+            self.val_col,
+        ]
