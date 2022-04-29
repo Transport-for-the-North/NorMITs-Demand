0d419c54aed1b463f54d38f9a2787355782495a9
Isaac <isaac.scott@wsp.com>
2022-04-29 16:24:32 +0100

    created NTEM growth process and included batch file to run it as it needs to change environments midway through.  Committing all at once.

---
+++ run_helper_scripts/NTEM_process/ntem.bat
@@ -0,0 +1,5 @@
+cd C:\Projects\MidMITS\Python\scripts\NorMITs-Demand\run_helper_scripts
+call C:/Users/ukiws001/Anaconda-32-bit/Scripts/activate
+C:/Users/ukiws001/Anaconda-32-bit/python.exe c:/Projects/MidMITS/Python/scripts/NorMITs-Demand/run_helper_scripts/mdb_read.py
+call C:/Users/ukiws001/Anaconda3/Scripts/activate
+C:/Users/ukiws001/Anaconda3/python.exe c:/Projects/MidMITS/Python/scripts/NorMITs-Demand/run_helper_scripts/NTEM_process.py
