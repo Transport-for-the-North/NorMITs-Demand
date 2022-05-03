5f62c8f3f1b6983d4390c71a7e915d726b136712
Matt <matthew.buckley@wsp.com>
2022-05-03 16:56:59 +0100

    Added Post ME adjustment graphs
    Added comparisons plots to the Post ME adjustment script.
    Fixed time period output of Post ME matrices by converting from
    average hour to full period.
    Added various docstrings.

--- run_helper_scripts/post_me_adjustment.py
+++ run_helper_scripts/post_me_adjustment.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-    Compares demand before and after SATURN's matrix estimation process.
-"""
+"""Compares demand before and after SATURN's matrix estimation process."""
 
 ##### IMPORTS #####
 # Standard imports
 import dataclasses
+import os
 import shutil
 import sys
 from pathlib import Path
 from typing import Iterator, NamedTuple, Union
 
 # Third party imports
+import geopandas as gpd
+import numpy as np
 import pandas as pd
+import seaborn
+from matplotlib import pyplot as plt
+from matplotlib.backends import backend_pdf
 
 # Local imports
 sys.path.append("..")
@@ -31,6 +35,7 @@ LOG = nd_log.get_logger(
 )
 LOG_FILE = "SATURN_ME_comparison.log"
 TIME_PERIODS = {1: "AM", 2: "IP", 3: "PM"}
+TIME_PERIOD_HOURS = {"AM": 3, "IP": 6, "PM": 3}
 USERCLASSES = {1: "business", 2: "commute", 3: "other"}
 COMPARISON_ZONING = "lad_2020"
 COMPARISON_SEGMENTATIONS = {"hb_p_m", "hb_p_m_tp_wday"}
@@ -56,7 +61,10 @@ class PostMEAdjustmentParameters:
     output_folder: Path
     model_name: str
     year: int
+    geospatial_file: Path
+    geospatial_columns: tuple[str, str] = ("lad_2020_zone_id", "LAD20CD")
     mode: nd.Mode = nd.Mode.CAR
+    simplify_geometry: int = 100
 
 
 class MatrixDetails(NamedTuple):
@@ -111,6 +119,10 @@ def process_post_me(
             lvl_nm = omx.matrix_levels[uc - 1]
             mat = omx.get_matrix_level(lvl_nm)
             mat = pd.DataFrame(mat, index=omx.zones, columns=omx.zones)
+
+            # Convert from average hour to period
+            mat = mat * TIME_PERIOD_HOURS[TIME_PERIODS[time_slice]]
+
             file_ops.write_df(mat, out)
             LOG.info("Written: %s", out)
 
@@ -136,7 +148,7 @@ def copy_prior_tp4(prior_folder: Path, out_folder: Path) -> None:
 
 
 def iter_od_matrices(prior_folder: Path, post_folder: Path) -> Iterator[MatrixDetails]:
-    """Iterates through the prior matrices and finds the relevant post ME matrix.
+    """Iterate through the prior matrices and finds the relevant post ME matrix.
 
     Logs a warning message if a Prior matrix is found without a
     correponding post ME matrix.
@@ -190,6 +202,27 @@ def _read_matrix(path: Path) -> pd.DataFrame:
 def production_trip_ends(
     prior_path: Path, post_path: Path
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
+    """Calculate the prior and post ME productions trip ends.
+
+    Parameters
+    ----------
+    prior_path : Path
+        Prior matrix file.
+    post_path : Path
+        Post ME matrix file.
+
+    Returns
+    -------
+    pd.DataFrame
+        Prior ME productions trip ends.
+    pd.DataFrame
+        Post ME productions trip ends.
+
+    Raises
+    ------
+    ValueError
+        If the prior and post matrices don't have the same zone system.
+    """
     prior = _read_matrix(prior_path)
     post = _read_matrix(post_path)
 
@@ -209,6 +242,26 @@ def production_trip_ends(
 def combined_production_trip_ends(
     prior_folder: Path, post_folder: Path, model_name: str, output_folder: Path
 ) -> tuple[nd.DVector, nd.DVector]:
+    """Combine the production trip ends from all purposes to two DVectors.
+
+    Saves the prior and post ME DVectors to `output_folder`.
+
+    Parameters
+    ----------
+    prior_folder : Path
+        Folder containing the prior matrices.
+    post_folder : Path
+        Folder containing the post ME matrices.
+    model_name : str
+        Name of the model zoning system.
+    output_folder : Path
+        Folder to save the DVectors to.
+
+    Returns
+    -------
+    tuple[nd.DVector, nd.DVector]
+        Prior and post ME DVectors.
+    """
     LOG.info("Creating productions DVectors")
     segmentation = nd.get_segmentation_level("hb_p_m_tp_wday")
     zoning = nd.get_zoning_system(model_name)
@@ -233,9 +286,33 @@ def combined_production_trip_ends(
     return prior_dvec, post_dvec
 
 
-def compare_productions(prior: nd.DVector, post: nd.DVector, output_path: Path) -> None:
-    MODE = nd.Mode.CAR
+def compare_productions(
+    prior: nd.DVector, post: nd.DVector, output_path: Path
+) -> dict[str, pd.DataFrame]:
+    """Calculate the post ME factor for each of `COMPARISON_SEGMENTATIONS`.
 
+    Outputs post ME / prior ME production trip ends at various
+    segmentation levels.
+
+    Parameters
+    ----------
+    prior : nd.DVector
+        Prior ME productions trip ends.
+    post : nd.DVector
+        Post ME productions trip ends.
+    output_path : Path
+        Path to save the output files to, segmentation and
+        mode name are appended to output different files.
+
+    Returns
+    -------
+    dict[str, pd.DataFrame]
+        Post ME factors DataFrames at various segmentation levels.
+    """
+    MODE = nd.Mode.CAR
+    LOG.info("Comparing productions")
+
+    comparisons = {}
     for seg in COMPARISON_SEGMENTATIONS:
         if seg != prior.segmentation.name:
             segmentation = nd.get_segmentation_level(seg)
@@ -254,9 +331,136 @@ def compare_productions(prior: nd.DVector, post: nd.DVector, output_path: Path) 
         file_ops.write_df(comp_df, out, index=False)
         LOG.info("Written: %s", out)
 
+        comparisons[seg] = comp_df
+
+    return comparisons
+
+
+def comparison_plots(comparisons: dict[str, pd.DataFrame], excel_output: Path) -> None:
+    """Produce summary comparison plots for the Post ME factors data.
+
+    Uses `excel_output` as the name for both the Excel and PDF outputs.
+
+    Parameters
+    ----------
+    comparisons : dict[str, pd.DataFrame]
+        Post ME factors data from `compare_productions`.
+    excel_output : Path
+        Excel file to save some output summaries to.
+    """
+    PALETTE = "muted"
+    LOG.info("Comparison plots")
+
+    with pd.ExcelWriter(excel_output) as excel:
+        with backend_pdf.PdfPages(excel_output.with_suffix(".pdf")) as pdf:
+            for seg_name in COMPARISON_SEGMENTATIONS:
+                df = comparisons[seg_name].drop(columns="m")
+                comp_column = "Time Period" if "tp" in df.columns else "NTEM Purpose"
+
+                df.rename(
+                    columns={"p": "NTEM Purpose", "tp": "Time Period", "val": "ME Factor"},
+                    inplace=True,
+                )
+                df = df.set_index([c for c in df.columns if c.lower().strip() != "me factor"])
+
+                # Write standard statistics to Excel for all values and by comparison column
+                df.describe().stack().unstack(0).to_excel(excel, sheet_name=seg_name)
+                df.unstack(comp_column).describe().stack().unstack(0).to_excel(
+                    excel, sheet_name=f"{seg_name} - {comp_column}"
+                )
+
+                if comp_column == "Time Period":
+                    df = df.drop(4, level="Time Period")
+
+                fig, axes = plt.subplots(
+                    2, 1, figsize=(10, 10), sharex=True, tight_layout=True
+                )
+                fig.suptitle(f"ME Factor Distribution at {seg_name}")
+
+                seaborn.kdeplot(x="ME Factor", ax=axes[0], data=df, color="black")
+                axes[0].set_title(f"Factors Across All {comp_column}")
+                seaborn.kdeplot(
+                    x="ME Factor", hue=comp_column, data=df, ax=axes[1], palette=PALETTE
+                )
+                axes[1].set_title(f"Factors by {comp_column}")
+                pdf.savefig(fig)
+                plt.close()
+
+                df.reset_index(inplace=True)
+                fig, ax = plt.subplots(figsize=(10, 10), tight_layout=True)
+                seaborn.boxplot(
+                    x=comp_column,
+                    y="ME Factor",
+                    data=df,
+                    ax=ax,
+                    palette=PALETTE,
+                    whis=np.inf,
+                )
+                seaborn.scatterplot(
+                    x=range(len(df[comp_column].unique())),
+                    y="ME Factor",
+                    data=df.groupby(comp_column)["ME Factor"].mean().to_frame(),
+                    ax=ax,
+                    color="black",
+                    label="Mean ME Factor",
+                    marker="X",
+                )
+                ax.set_title(f"ME Factors by {comp_column}")
+                pdf.savefig(fig)
+                plt.close()
+
+        LOG.info("Written: %s", excel_output)
+
+
+def join_geodata(
+    comparisons: pd.DataFrame,
+    output_path: Path,
+    geospatial_file: Path,
+    geo_columns: tuple[str, str],
+    simplify_geometry: int = None,
+) -> None:
+    """Join the geospatial polygon data to the comparisons.
+
+    Parameters
+    ----------
+    comparisons : pd.DataFrame
+        Post ME factors data from `compare_productions`
+        for a single segmentation.
+    output_path : Path
+        Path to save the output file to.
+    geospatial_file : Path
+        Geospatial file of the comparisons zone system.
+    geo_columns : tuple[str, str]
+        Columns to join the data on, the first name should
+        be for the `comparisons` column and the second for the
+        `geospatial_file` column.
+    simplify_geometry : int, optional
+        Simlify the geometry with the given tolerance, if None
+        no simlification is done.
+    """
+    geospatial = gpd.read_file(geospatial_file)
+    geospatial = geospatial.loc[:, [geo_columns[1], "geometry"]]
+    if simplify_geometry is not None:
+        geospatial.loc[:, "geometry"] = geospatial.simplify(simplify_geometry)
+
+    # Fixes the inf error when converting CRS with geopandas
+    os.environ["PROJ_NETWORK"] = "OFF"
+    geospatial = geospatial.to_crs("WGS84")
+
+    comp = comparisons.merge(
+        geospatial,
+        left_on=geo_columns[0],
+        right_on=geo_columns[1],
+        how="left",
+        validate="m:1",
+    )
+    out = output_path.with_name(output_path)
+    comp.to_csv(out, index=False)
+    LOG.info("Written: %s", out)
+
 
 def main(params: PostMEAdjustmentParameters, init_logger: bool = True) -> None:
-
+    """Compare the prior and post ME matrices and calculate adjustment factors."""
     params.output_folder.mkdir(exist_ok=True, parents=True)
     if init_logger:
         nd_log.get_logger(
@@ -297,12 +501,23 @@ def main(params: PostMEAdjustmentParameters, init_logger: bool = True) -> None:
 
     out_nm = "prior_post_comparison_productions_{}"
     out_path = compare_out / out_nm.format(params.model_name)
-    compare_productions(prior_dvec, post_dvec, out_path)
+    comparisons = compare_productions(prior_dvec, post_dvec, out_path)
+    comparison_plots(comparisons, out_path.with_suffix(".xlsx"))
+
     comp_zoning = nd.get_zoning_system(COMPARISON_ZONING)
-    compare_productions(
+    out_path = out_path.with_name(out_nm.format(comp_zoning.name))
+    comparisons = compare_productions(
         prior_dvec.translate_zoning(comp_zoning),
-        prior_dvec.translate_zoning(comp_zoning),
-        out_path.with_name(out_nm.format(comp_zoning.name)),
+        post_dvec.translate_zoning(comp_zoning),
+        out_path,
+    )
+    comparison_plots(comparisons, out_path)
+    join_geodata(
+        comparisons["hb_p_m"],
+        out_path.with_name(out_path.stem + "-hb_p_m-geospatial.csv"),
+        params.geospatial_file,
+        params.geospatial_columns,
+        params.simplify_geometry,
     )
 
 
@@ -339,5 +554,10 @@ if __name__ == "__main__":
         ),
         model_name="miham",
         year=2018,
+        geospatial_file=Path(
+            r"Y:\Data Strategy\GIS Shapefiles"
+            r"\Local_Authority_Districts_(December_2020)_UK_BFC"
+            r"\Local_Authority_Districts_(December_2020)_UK_BGC.shp"
+        ),
     )
     main(parameters)
