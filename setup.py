a94c39c6b10faa1bddf4df7610972b21c53de1b9
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2022-08-09 16:21:17 +0100

    Auto versioning setup
    Setup of versioneer ran

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
-from pathlib import Path
 from setuptools import setup
+import versioneer
 
 # BACKLOG: Properly integrate setup.py
 #  labels: demand merge
-
-
-def get_version(pkg_name):
-    version_filename = Path(__file__).parent / pkg_name / "version.py"
-    with open(version_filename) as f:
-        for line in f.readlines():
-            if line.startswith("__version__"):
-                sep = '"' if '"' in line else "'"
-                return line.split(sep)[1]
-    raise RuntimeError(f"Version not found in {version_filename}")
-
 
 PACKAGE_NAME = "normits_demand"
 
 setup(
     name=PACKAGE_NAME,
-    version=get_version(PACKAGE_NAME),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="Add description of NorMITs Demand",
     packages=[PACKAGE_NAME],
 )
