9db128d3c41aa5153470c400e9b17bc9e56cdb91
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2022-08-10 08:27:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup
 import versioneer
 
-# BACKLOG: Properly integrate setup.py
-#  labels: demand merge
-
 PACKAGE_NAME = "normits_demand"
 
 setup(
     name=PACKAGE_NAME,
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description="Add description of NorMITs Demand",
+    description="Transport for the North's synthetic demand tools",
     packages=[PACKAGE_NAME],
 )
