770efb2e5f97b0943d23b33eb8d6b95e118dce2e
mishtaiwi <113102424+mishtaiwi@users.noreply.github.com>
2022-10-12 10:23:37 +0100

    Add files via upload

--- normits_demand/models/forecasting/edge_replicant.py
+++ normits_demand/models/forecasting/edge_replicant.py
@@ -9,6 +9,7 @@
 # Local imports
 from normits_demand import logging as nd_log
 from normits_demand.models.forecasting import forecast_cnfg
+from normits_demand.models.forecasting import edge_forecast
 
 ##### CONSTANTS #####
 LOG = nd_log.get_logger(__name__)
@@ -17,5 +18,6 @@ LOG = nd_log.get_logger(__name__)
 
 ##### FUNCTIONS #####
 def main(params: forecast_cnfg.EDGEParameters) -> None:
-    """Main function for running the EDGE model."""
+    edge_forecast.RunEDGEGrowth(params)
     raise NotImplementedError("Not yet implemented main function for EDGE model")
+    
