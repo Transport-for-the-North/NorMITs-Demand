13e936fee38cc3da434126e032f0e98079f1c14f
BenTaylor-TfN (Sneezy) <benjamin.taylor@transportforthenorth.com>
2023-01-05 08:34:00 +0000

    Tidy up config folder

--- config/tempro/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import pandas as pd
-
-if __name__ == '__main__':
-    code_to_zone = pd.read_csv('ntem_code_to_zone.csv')
-    geo_lookup = pd.read_csv('tblLookupGeo76.csv')
